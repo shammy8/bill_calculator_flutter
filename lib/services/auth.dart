778793e42df137bcabfa81ed004c947406b36904
Allan Sham <allansham@hotmail.com>
2021-07-09 23:20:17 +0100

    Update google sign in to null safety version

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,6 +1,4 @@
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:firebase_auth/firebase_auth.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:cloud_firestore/cloud_firestore.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:google_sign_in/google_sign_in.dart';
@@ -11,24 +9,27 @@ class AuthService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  Stream<User> get user => _auth.authStateChanges();
+  Stream<User?> get user => _auth.authStateChanges();
 
-  Future<User> anonLogin() async {
+  Future<User?> anonLogin() async {
     UserCredential user = await _auth.signInAnonymously();
     // updateUserData(user)
     return user.user;
   }
 
-  Future<User> googleSignIn() async {
+  Future<User?> googleSignIn() async {
     // try {
-    final GoogleSignInAccount googleSignInAccount =
+    final GoogleSignInAccount? googleSignInAccount =
         await _googleSignIn.signIn();
+    if (googleSignInAccount == null) {
+      return null;
+    }
     final GoogleSignInAuthentication googleAuth =
         await googleSignInAccount.authentication;
     final AuthCredential credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
     UserCredential result = await _auth.signInWithCredential(credential);
-    User user = result.user;
+    User? user = result.user;
     // updateUserData(user)
     return user;
     // } catch (error) {
