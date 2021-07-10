a9a7b8e060888eb74834dc3465a3a84d0e0c15d2
Allan Sham <allansham@hotmail.com>
2021-07-10 11:20:06 +0100

    Create a login screen and add log out button in Bill.dart

--- lib/screens/bill.dart
+++ lib/screens/bill.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:bill_calculator_flutter/services/auth.dart';
 
 class BillScreen extends StatelessWidget {
+  final AuthService auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Bill'),
+      appBar: AppBar(
+        title: Text('Bill'),
+      ),
+      body: (Container(
+        child: ElevatedButton(
+            child: Text('Log out'),
+            onPressed: () {
+              auth.signOut();
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/', (route) => false);
+            }),
+      )),
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,10 +1,107 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class LoginScreen extends StatelessWidget {
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:bill_calculator_flutter/services/auth.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  AuthService auth = AuthService();
+
+  late StreamSubscription<User?> unsubscribe; // TODO is using late correct here
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO unsubscribe
+    unsubscribe = auth.user.listen(
+      (user) {
+        if (user != null) {
+          Navigator.pushReplacementNamed(context, '/bill');
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    unsubscribe.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('hi'),
+      body: Container(
+        padding: EdgeInsets.all(30),
+        decoration: BoxDecoration(),
+        child: Column(
+          children: [
+            FlutterLogo(
+              size: 150,
+            ),
+            Text(
+              'Login to Start',
+              style: Theme.of(context).textTheme.headline5,
+              textAlign: TextAlign.center,
+            ),
+            LoginButton(
+              text: 'Login with Google',
+              icon: FontAwesomeIcons.google,
+              color: Colors.black,
+              loginMethod: auth.googleSignIn,
+            ),
+            LoginButton(
+              text: 'Login as Guest',
+              loginMethod: auth.anonLogin,
+              icon: Icons.face,
+            ),
+          ],
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final String? text;
+  final IconData? icon;
+  final Color? color;
+  final Function loginMethod;
+
+  LoginButton(
+      {required this.text, this.icon, this.color, required this.loginMethod});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      child: TextButton.icon(
+        // padding: EdgeInsets.all(30),
+        onPressed: () async {
+          var user = await loginMethod();
+          if (user != null) {
+            Navigator.pushReplacementNamed(context, '/bill');
+          }
+        },
+        icon: Icon(icon, color: Colors.white),
+        style: TextButton.styleFrom(
+            backgroundColor: color, padding: EdgeInsets.all(30)),
+        label: Expanded(
+          child: Text(
+            '$text',
+            textAlign: TextAlign.center,
+            textScaleFactor: 1.3,
+          ),
+        ),
+      ),
     );
   }
 }
