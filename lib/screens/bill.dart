e2e4af4415ecb9719011e200c56c0a26dfe514e3
Allan Sham <allansham@hotmail.com>
2021-07-13 00:14:29 +0100

    Change the bill widget to use slivers. Add in all the sliders

--- lib/screens/bill.dart
+++ lib/screens/bill.dart
@@ -32,30 +32,55 @@ class BillScreen extends StatelessWidget {
       ],
       builder: (context, child) {
         final List<Bill> bills = context.watch<List<Bill>>();
+        final items = context.watch<List<Item>>();
         if (billId != 'empty') {
           Bill bill = bills.firstWhere((bill) => bill.uid == billId);
           return Scaffold(
-            appBar: AppBar(),
             endDrawer: BillDrawer(),
-            body: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(bill.name,
-                          style: Theme.of(context).textTheme.headline5),
-                      ElevatedButton.icon(
-                        onPressed: () {},
-                        icon: Icon(Icons.add),
-                        label: Text('Add Item'),
-                      )
-                    ],
+            body: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  pinned: true,
+                  floating: true,
+                  expandedHeight: 240,
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: Text(bill.name),
+                    background: Image.network(
+                      'https://thumbs-prod.si-cdn.com/6MmkyNmsODbteKNYiAMz6201KOw=/800x600/filters:no_upscale():focal(2062x722:2063x723)/https://public-media.si-cdn.com/filer/20/85/2085a351-1759-4b7e-bb3e-eadec17b7aef/papageitaucher_fratercula_arctica.jpg',
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  Items()
-                ],
-              ),
+                ),
+                SliverPadding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        return Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(items[index].description,
+                                    style:
+                                        Theme.of(context).textTheme.headline6),
+                                Text(items[index].cost.toString(),
+                                    style:
+                                        Theme.of(context).textTheme.headline6),
+                              ],
+                            ),
+                            TwoSidedCheckableList(
+                              sharedBy: items[index].sharedBy,
+                            ),
+                            Divider(),
+                          ],
+                        );
+                      },
+                      childCount: items.length,
+                    ),
+                  ),
+                ),
+              ],
             ),
           );
         } else {
@@ -68,6 +93,39 @@ class BillScreen extends StatelessWidget {
           );
         }
       },
+    );
+  }
+}
+
+class TwoSidedCheckableList extends StatelessWidget {
+  // final void Function(Friend) onTap;
+  final List<SharedBy> sharedBy;
+
+  TwoSidedCheckableList({required this.sharedBy});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        for (var i = 0; i < sharedBy.length; i += 2)
+          Row(
+            children: [
+              Switch(
+                onChanged: (v) {},
+                value: sharedBy[i].settled,
+              ),
+              Text(sharedBy[i].friend),
+              Spacer(),
+              if (i + 1 < sharedBy.length) //
+                Text(sharedBy[i + 1].friend),
+              if (i + 1 < sharedBy.length)
+                Switch(
+                  onChanged: (v) {},
+                  value: sharedBy[i].settled,
+                ),
+            ],
+          ),
+      ],
     );
   }
 }
